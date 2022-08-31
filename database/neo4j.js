8ecc28c8c950bfcfb8788dba6f546721fd7d1f62
Lucas Tavares <zlucas.np@gmail.com>
2022-08-31 19:20:45 -0300

    refactor

--- database/neo4j.js
+++ database/neo4j.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const neo4j = require('neo4j-driver');
 const driver = new neo4j.driver(`neo4j+s://${process.env.NEO4J_HOST}`, neo4j.auth.basic(`${process.env.NEO4J_USER}`, `${process.env.NEO4J_PASSWORD}`));
+console.log("Neo4j conectado!")
 
 module.exports = driver;
